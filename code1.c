e78dcd3a6256778a17dd7850ca2ee8b61c156aca
Tanishq Tyagi <Tanishq.22099@stu.upes.ac.in>
2025-08-23 12:11:40 +0530

    Add files via upload

---
+++ code1.c
@@ -0,0 +1,15 @@
+// Write a program to input two numbers and display their sum
+
+#include<stdio.h>
+int main() {
+    int a,b,c;
+    printf("enter a");
+    scanf("%d", &a);
+    printf("enter b");
+    scanf("%d", &b);
+    c = a + b;
+    printf("%d", c);
+    return 0;
+
+
+}
