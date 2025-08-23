8db5323ebe168831bc9f9cb9c8bd9d7dab382a7f
Tanishq Tyagi <Tanishq.22099@stu.upes.ac.in>
2025-08-23 14:56:36 +0530

    Update code6.c

--- code6.c
+++ code6.c
@@ -4,7 +4,7 @@
 int main() {
     int a, b, temp;
 
-    // Get input from the user
+
     printf("Enter a: ");
     scanf("%d", &a);
     printf("Enter b: ");
@@ -14,10 +14,9 @@ int main() {
     printf("a = %d\n", a);
     printf("b = %d\n", b);
 
-    // Swap logic using a third variable
-    temp = a; // Store the value of 'a' in 'temp'
-    a = b;    // Assign the value of 'b' to 'a'
-    b = temp; // Assign the value of 'temp' (original 'a') to 'b'
+    temp = a; 
+    a = b;    
+    b = temp; 
 
     printf("\nAfter swapping:\n");
     printf("a = %d\n", a);
@@ -25,3 +24,4 @@ int main() {
 
     return 0;
 }
+
