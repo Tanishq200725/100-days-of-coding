8503649c9ae1a749ad29ce4be42fd4ed656aca21
Tanishq Tyagi <Tanishq.22099@stu.upes.ac.in>
2025-09-14 13:59:38 +0530

    Add files via upload

---
+++ code46.c
@@ -0,0 +1,26 @@
+//Write a program to print the following pattern:
+//*****
+//*****
+//*****
+//*****
+//*****
+#include<stdio.h>
+int main(){
+int i,n,j;
+printf("enter number of rows:");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+    for(j=1;j<=n;j++){
+        printf("*");
+    }
+    printf("\n");
+}
+return 0;
+
+
+
+
+
+
+
+}
